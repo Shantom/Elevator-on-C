ff6364abf3361a9cbdb5caf87756564f19525b40
Shantom <1044950475@qq.com>
2015-05-16 10:58:35 +0800

    state

--- elevator/State.c
+++ elevator/State.c
@@ -6,18 +6,18 @@
 //////////////////////
 #include"elevator.h"
 
-extern void judgeUpDown_State（int currentFloor，int goalFloor）;//状态判断
-extern void  judgeGoalFloor1_State（list *）;//策略1目标楼层判断
-extern void  judgeGoalFloor2_State（list *）;//策略2目标楼层判断
-extern void  judgeGoalFloor3_State（list *）;//策略3目标楼层判断
-extern void  judgeGoalFloor4_State（list *）;//策略4目标楼层判断
+extern void judgeUpDown_State(int currentFloor,int goalFloor);//状态判断
+extern void  judgeGoalFloor1_State(list *);//策略1目标楼层判断
+extern void  judgeGoalFloor2_State(list *);//策略2目标楼层判断
+extern void  judgeGoalFloor3_State(list *);//策略3目标楼层判断
+extern void  judgeGoalFloor4_State(list *);//策略4目标楼层判断
 extern void  timeRecord_State();//时间记录函数
 
 /*
 *状态判断
 */
 void
-judgeUpDown_State（int currentFloor，int goalFloor）
+judgeUpDown_State(int currentFloor,int goalFloor)
 {
 	if (currentFloor < goalFloor)
 	    upper_Run();//电梯上行
@@ -31,7 +31,7 @@ judgeUpDown_State（int currentFloor，int goalFloor）
 /*
 *策略1目标楼层判断
 */
-void  judgeGoalFloor1_State（list **allCmd）
+void  judgeGoalFloor1_State(list **allCmd)
 {
 	goalFloor = (**allCmd).floorNumber;
 	(*allCmd) = (*allCmd)->next;
@@ -41,7 +41,7 @@ void  judgeGoalFloor1_State（list **allCmd）
 *策略2目标楼层判断
 */
 
-void  judgeGoalFloor2_State（）
+void  judgeGoalFloor2_State()
 {
 	if (elevState == UP)
 	{
@@ -62,8 +62,8 @@ void  judgeGoalFloor2_State（）
 		
 	}
 }
-void  judgeGoalFloor3_State（list *）
-void  judgeGoalFloor4_State（list *）
+void  judgeGoalFloor3_State(list *)
+void  judgeGoalFloor4_State(list *)
 void  timeRecord_State()
 
 
